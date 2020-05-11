2d36995748ad3a2a08f9e257ca34b1e16255e8c9
Giovani Pinke <giovani.pinke@hotmail.com>
2020-05-10 22:34:36 -0300

    Adding LoginScreen

--- src/routes/AppNavigator.js
+++ src/routes/AppNavigator.js
@@ -3,13 +3,19 @@ import {View, Text} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import LoginFlowScreen from '../screens/login/loginFlow/LoginFlowScreen';
+import LoginScreen from '../screens/login/LoginScreen';
 
 const AppStack = createStackNavigator();
 
 function AppNavigator() {
   return (
     <AppStack.Navigator headerMode="none">
+      
       <AppStack.Screen name="Flow" component={LoginFlowScreen} />
+
+      <AppStack.Screen name="Login" component={LoginScreen} />
+
+
     </AppStack.Navigator>
   );
 }
---
+++ src/screens/login/styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components/native';
+
+import * as ScaleUtils from '../../utils/scale';
+
+export const LoginFlowContainerView = styled.View`
+    flex: 1;
+    background-color: #FFFF;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10% 8%;
+`;
+
+export const LogoView = styled.View`
+    margin-top: 10%;
+`;
+
+
+export const AppNameText = styled.Text`
+    font-size: 48px;
+    font-family: Poppins-Bold;
+    text-align: center;
+`;
+
+export const CustomLetterText = styled.Text`
+    color: #4F80E1;
+`;
+
+export const WelcomeView = styled.View`
+    margin-bottom: 10%;
+`;
+
+export const WelcomeHeaderTitleText = styled.Text`
+    font-size: 24px;
+    text-align: center;
+    font-family: Poppins-Bold;
+    margin-bottom: 5%;
+`;
+
+export const WelcomeDescriptionText = styled.Text`
+    font-size: 14px;
+    font-family: Poppins-Regular;
+    text-align: center;
+    margin-bottom: 10%;
+`;
+
+export const ButtonAreaView = styled.View`
+    height: ${ScaleUtils.ScreenHeight * 0.15}px;
+    justify-content: space-around;
+`;
